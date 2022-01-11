b8cd0ac99ff9d26879f442e8c0c5dafb37ba27f3
strimell <1vampirus1@gmail.com>
2022-01-11 22:55:06 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import AgileBoard from "./compontents/AgileBoard/AgileBoard"
+
+const App: React.FC = props => {
+    return (
+        <div>
+            <AgileBoard />
+        </div>
+    )
+}
+
+export default App
---
+++ src/compontents/AgileBoard/AgileBoard.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from "./AgileBoard.module.scss"
+
+const AgileBoard: React.FC = props => {
+    return <h1 className={styles.agileBoard}>Agile board</h1>
+}
+
+export default AgileBoard
